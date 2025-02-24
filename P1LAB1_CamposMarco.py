844f0f94254f6c356331600f4cc7e1463dddb96f
Camposlm1566 <camposlm1566@student.faytechcc.edu>
2025-02-23 21:37:09 -0500

    Add files via upload
    Creating Python using IDLE

---
+++ P1LAB1_CamposMarco.py
@@ -0,0 +1,12 @@
+"Marco_Campos"
+2/23/2025
+"P1LAB1"
+"Creating_a_Python_program_using_IDLE"
+
+first_name = input("Enter your first name : ")
+last_name = input("Enter your last name : ")
+
+print(f"{first_name}")
+print(f"{last_name}")
+
+print("Hello! Welcome to CTI-110")
