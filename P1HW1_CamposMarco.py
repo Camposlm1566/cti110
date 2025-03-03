c84e880dd189f85318d4e48d3c2e7dded878780b
Camposlm1566 <camposlm1566@student.faytechcc.edu>
2025-03-03 00:29:52 +0000

    New HW file

---
+++ P1HW1_CamposMarco.py
@@ -0,0 +1,19 @@
+#Marco Campos
+#March 2, 2025
+#P1HW1
+#Creating a program that allows output 
+
+# Calculating Exponents
+print("-----Calculating Exponents----")
+base = int(input("\nEnter an integer as the base valuve: 7 "))
+exponent = int(input("Enter an integer as the exponent: 3 "))
+result = base ** exponent
+print(f"\n{base} 7 raised to the power of 3 {exponent} is 343 {result} !!\n")
+
+# Addition and Subtraction
+print("-----Addition and Subtraction----")
+num1 = int(input("\nEnter a starting integer: 10 "))
+num2 = int(input("Enter an integer to add: 6 "))
+num3 = int(input("Enter an integer to subtract: 4 "))
+final_result = num1 + num2 - num3
+print(f"\n{num1} + {num2} - {num3} is equal to {final_result}")
