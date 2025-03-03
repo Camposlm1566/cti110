ba98e6252f57bf1e754e3da3879b509fe93a4e80
Camposlm1566 <camposlm1566@student.faytechcc.edu>
2025-03-02 20:22:56 -0500

    Add files via upload
    New HW asssignment

---
+++ P1HW2_CamposMarco.py
@@ -0,0 +1,47 @@
+# Marco Campos
+# March 2, 2025
+# P1HW2
+# Creating a program with simple budget
+
+# Pseudocode:
+# 1. Start
+# 2. Ask the user to enter their budget
+# 3. Ask the user to enter their travel destination
+# 4. Ask the user for the amount they will spend on gas
+# 5. Ask the user for the amount they will spend on accommodation
+# 6. Ask the user for the amount they will spend on food
+# 7. Calculate total expenses by adding gas, accommodation, and food expenses
+# 8. Subtract total expenses from budget to get the remaining budget
+# 9. Display the travel destination, total expenses, and remaining budget
+# 10. End
+
+# Ask user to enter their budget
+budget = float(input("Enter your budget: "))
+
+# Ask user to enter travel destination
+destination = input("Enter your travel destination: ")
+
+# Ask user for amount they will spend on gas
+gas_expense = float(input("Enter the amount you will spend on gas: "))
+
+# Ask user for amount they will spend on accomodation
+accomodation_expense = float(input("Enter the amount you will spend on  accomodation: "))
+
+# Ask user for amount they will spend on food
+food_expense = float(input("Enter the amount you will spend on food: "))
+
+# Add Expenses
+total_expenses = gas_expense + accomodation_expense + food_expense
+
+#Subtract expenses from budget
+remaining_budget = budget - total_expenses
+
+# Display results
+print(f"\nTravel Destination: {destination}")
+print(f"Total Expenses: ${total_expenses: .2f}")
+print(f"Remaining Budget: ${remaining_budget: .2f}")
+
+
+
+
+
